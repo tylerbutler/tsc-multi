cb3591d9507f0f819405fd211b18c90f26ce7893
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-12-20 21:56:54 +0000

    Rewrite dynamic imports in type declaration files

--- src/transformers/rewriteDtsImport.ts
+++ src/transformers/rewriteDtsImport.ts
@@ -18,6 +18,8 @@ export function createRewriteDtsImportTransformer(
     isStringLiteral,
     isImportDeclaration,
     isCallExpression,
+    isMappedTypeNode,
+    isTemplateLiteralTypeSpan,
     SyntaxKind,
     visitNode,
     visitEachChild,
@@ -62,7 +64,24 @@ export function createRewriteDtsImportTransformer(
   return (ctx) => {
     let sourceFile: ts.SourceFile;
 
+    const recentNodes: ts.Node[] = [];
+
     const visitor: ts.Visitor = (node) => {
+      // Add current node to the beginning of the list of recent nodes; keep 3 max.
+      if (recentNodes.unshift(node) > 3) {
+        recentNodes.pop();
+      }
+
+      // if (sourceFile.fileName.includes("schemaChangeFormat")) {
+      //   console.log(
+      //     `VISITOR [nodeKind: ${node.kind}, parent: ${
+      //       node.parent?.kind
+      //     }, text: ${(node as any)?.text}, pos: ${node.pos}]`
+      //   );
+      //   console.log(
+      //     `RECENT_NODES: ${recentNodes[0]?.kind} ${recentNodes[1]?.kind} ${recentNodes[2]?.kind}`
+      //   );
+      // }
       // ESM import
       if (isImportDeclaration(node)) {
         return factory.createImportDeclaration(
@@ -84,6 +103,77 @@ export function createRewriteDtsImportTransformer(
           updateModuleSpecifier(ctx, sourceFile, node.moduleSpecifier),
           node.assertClause
         );
+      }
+
+      // Extremely britle way to detect dynamic imports that aren't caught by
+      //   isCallExpression(node) && node.expression.kind === SyntaxKind.ImportKeyword
+      //
+      // They come up in some cases when we don't do explicit typing in source code and the type declaration files
+      // end up looking something like this:
+      //
+      //  export declare const EncodedSchemaChange: import("@sinclair/typebox").TObject<{
+      //    new: import("@sinclair/typebox").TObject<{
+      //      version: import("@sinclair/typebox").TLiteral<1>;
+      //      nodes: import("@sinclair/typebox").TRecord<import("@sinclair/typebox").TString, import("@sinclair/typebox").TObject<{
+      //      ...
+      //
+      // Tree has several cases, but the fully-defined explicit types are a nightmare to maintain.
+      if (recentNodes.length == 4) {
+        // console.log(
+        //   `CHECKING: ${isStringLiteral(node)} ${isMappedTypeNode(
+        //     recentNodes[1]
+        //   )} ${isTemplateLiteralTypeSpan(recentNodes[2])}`
+        // );
+        if (
+          isStringLiteral(node) &&
+          isMappedTypeNode(recentNodes[1]) &&
+          isTemplateLiteralTypeSpan(recentNodes[2])
+        ) {
+          // The current node is the argument to a dynamic import (i.e. `import("../..")`) of a particular kind that is
+          // not caught by other conditions in this file. We need to transform the string literal to a valid path
+          // with a file name and extension so the ESM build doesn't complain.
+
+          // if (sourceFile.fileName.includes("testRecursiveDomain")) {
+          //   console.log(
+          //     `sourceFile: ${sourceFile.fileName}, ${
+          //       node.text
+          //     }, isRelativePath: ${isRelativePath(
+          //       node.text
+          //     )}, isDirectory: ${isDirectory(
+          //       sourceFile,
+          //       node.text
+          //     )}, extname: ${extname(node.text)}`
+          //   );
+          // }
+
+          // Ignore if it's not a relative path. ".." is a relative path for our purposes but is not considered as such by
+          // isRelativePath(), so we need to let that exception fall through to the code below.
+          if (node.text !== ".." && !isRelativePath(node.text)) return node;
+
+          // Ignore if it's a JSON import
+          const ext = extname(node.text);
+          if (ext === JSON_EXT && ctx.getCompilerOptions().resolveJsonModule) {
+            return node;
+          }
+
+          let updatedModulePath: ts.StringLiteral;
+          if (isDirectory(sourceFile, node.text)) {
+            // Add /index.<ext> if it's a directory
+            updatedModulePath = factory.createStringLiteral(
+              `${node.text}/index${options.extname}`
+            );
+          } else {
+            // Replace/add the extension if it's a file
+            const base =
+              ext === JS_EXT ? trimSuffix(node.text, JS_EXT) : node.text;
+
+            updatedModulePath = factory.createStringLiteral(
+              `${base}${options.extname}`
+            );
+          }
+          // console.log(`UPDATED ${node.text} to ${updatedModulePath.text}`);
+          return updatedModulePath;
+        }
       }
 
       // ESM dynamic import
